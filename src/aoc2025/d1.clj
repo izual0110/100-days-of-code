99dea01d7675b887a1acb1871fde5a7e2b743f47
Maxim Tsoy <mtsoy1990@gmail.com>
2025-12-01 15:12:04 +0900

    d1 2025

---
+++ src/aoc2025/d1.clj
@@ -0,0 +1,49 @@
+(ns aoc2025.d1
+  (:require [clojure.string :as str]
+            [utils :as u]))
+
+(def test_pswd (str/split (slurp "resources/aoc2025/1t.txt") #"\n"))
+(def pswd (str/split (slurp "resources/aoc2025/1.txt") #"\n"))
+
+
+(defn parse [n]
+  (let [[_ r d] (re-matches #"(\w)(\d*)" n)
+        d (Integer/parseInt d)]
+    (if (= r "R") d (- d))))
+
+(defn calc [n]
+  (loop [n n a 50 c 0]
+    (if (empty? n)
+      c
+      (recur (next n)  (mod (+ (first n) a) 100) (if (= 0 a) (inc c) c)))))
+
+
+(assert (= 3  (->> test_pswd
+                   (map parse)
+                   calc)))
+
+(assert (= 1040  (->> pswd
+                      (map parse)
+                      calc)))
+
+(defn calc2 [n]
+  (loop [n n a 50 c 0]
+    (if (empty? n)
+      c
+      (let [t (+ (first n) a)
+            d (abs (long (/ t 100)))
+            c (cond
+                (and (< t 0) (not= a 0)) (+ 1 c d)
+                (and (< t 0) (= a 0)) (+ c d)
+                (> t 0) (+ c d)
+                (= t 0) (inc c)
+                :else c)]
+        (recur (next n) (mod t 100) c)))))
+
+(assert (= 6 (->> test_pswd
+                  (map parse)
+                  calc2)))
+
+(assert (= 6027 (->> pswd
+                  (map parse)
+                  calc2)))
