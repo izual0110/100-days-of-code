b5773c5b377da19c1f3916622e366e76852d1292
Maxim Tsoy <mtsoy1990@gmail.com>
2025-12-08 02:26:29 +0900

    add utils

--- src/repl.clj
+++ src/repl.clj
@@ -17,7 +17,24 @@
   ([] (start default-opts))
   ([opts]
    (set! *warn-on-reflection* true)
-   (let [args (-> default-opts 
-                  (merge opts) 
+   (let [args (-> default-opts
+                  (merge opts)
                   opts->args)]
      (apply nrepl/-main args))))
+
+
+(defn init-day [d]
+  (let [ns-name (symbol (str "aoc2025.d" d))
+        file-path (str "src/aoc2025/d" d ".clj")
+        resource-path (str "resources/aoc2025/" d "t.txt")]
+    (if (or (.exists (clojure.java.io/file file-path))
+            (.exists (clojure.java.io/file resource-path)))
+      (println (str "Day " d " already exists, aborting."))
+      (do
+        (spit file-path (format "(ns aoc2025.d%s\n  (:require [clojure.string :as str]\n            [utils :as u]\n            [clj-async-profiler.core :as prof]))\n" d))
+        (spit resource-path "")
+        (println (str "Created " file-path))
+        (println (str "Created " resource-path))))
+    (require ns-name)))
+
+(comment (init-day 8))
