173758b4f64cfaf464f0f0313f800bac80765469
Maxim Tsoy <mtsoy1990@gmail.com>
2025-12-09 01:12:18 +0900

    refactoring

--- src/aoc2025/d8.clj
+++ src/aoc2025/d8.clj
@@ -26,12 +26,12 @@
 
 (defn group-boxes [connections boxes]
   (let [pairs (->> (for [[i v1] (map-indexed vector boxes)
-                          v2 (drop (inc i) boxes)]
-                      [[v1 v2] (distance v1 v2)])
-                    (sort-by second)
-                    (take connections)
-                    (map first))]
-    (loop [[[f1 f2] & rest-pairs] pairs r (into #{} (map hash-set) boxes)]
+                         v2 (drop (inc i) boxes)]
+                     [[v1 v2] (distance v1 v2)])
+                   (sort-by second)
+                   (take connections)
+                   (map first))]
+    (loop [[[f1 f2] & rest-pairs] pairs r (into #{} (pmap hash-set boxes))]
       (if (nil? f1)
         r
         (let [g1 (find-group r f1)
@@ -52,53 +52,41 @@
 
 
 (assert (= 52668 (time (->> boxes
-                            (mapv (parse-box))
+                            (map (parse-box))
                             (group-boxes 1000)
                             (map count)
                             sort
                             (take-last 3)
                             (apply *)))))
 
-(defn find-group2 [groups v]
-  (->> groups
-       (filter (fn [e]
-                 (contains? (->> e second) v)))
-       first))
-
-(defn group-boxes2 [boxes]
-  (let [groups (->> boxes
-                    (map-indexed (fn [i v1]
-                                   (mapv (fn [v2]
-                                           (if (< (first v1) (first v2))
-                                             [[v1 v2] (distance v1 v2)]
-                                             [[v2 v1] (distance v1 v2)]))
-                                         (nthnext boxes (inc i)))))
-                    (mapcat identity)
-                    (sort-by second))]
-    (loop [distances (map first groups) r (set (map (fn [v] [v #{v}]) boxes)) last-value (first distances)]
-      (if (empty? distances)
-        last-value
-        (let [f (first distances)
-              f1 (first f)
-              f2 (second f)
-              g1 (find-group2 r f1)
-              g2 (find-group2 r f2)
-              new-r (if (= g1 g2) r (-> r
-                                        (disj g1 g2)
-                                        (conj [(first g1) (apply conj (second g1) (second g2))])))]
-          (recur (next distances) new-r (if (= g1 g2) last-value f)))))))
+(defn last-pair [boxes]
+  (let [pairs (->> (for [[i v1] (map-indexed vector boxes)
+                         v2 (drop (inc i) boxes)]
+                     [[v1 v2] (distance v1 v2)])
+                   (sort-by second)
+                   (map first))]
+    (loop [[[f1 f2] & rest-pairs] pairs r (into #{} (map hash-set boxes)) last-pair [f1 f2]]
+      (if (nil? f1)
+        last-pair
+        (let [g1 (find-group r f1)
+              g2 (find-group r f2)]
+          (if (= g1 g2)
+            (recur rest-pairs r last-pair)
+            (recur rest-pairs (-> r
+                                  (disj g1 g2)
+                                  (conj (apply conj g1 g2))) [f1 f2])))))))
 
 
 
 
-(->> test-boxes
-     (mapv (parse-box))
-     (group-boxes2)
-     (map first)
-      (apply *))
+(assert (= 25272 (->> test-boxes
+                      (mapv (parse-box))
+                      last-pair
+                      (map first)
+                      (apply *))))
 
-(->> boxes
-     (mapv (parse-box))
-     (group-boxes2)
-     (map first)
-     (apply *))
+(assert (= 1474050600 (time (->> boxes
+                                 (map (parse-box))
+                                 last-pair
+                                 (map first)
+                                 (apply *)))))
